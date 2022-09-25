f9fda0c56413b17edb4bf2c842cfc6ef739c278c
Daza <williamdaza04@gmail.com>
2022-09-25 17:33:47 -0500

    Se agregan las rutas

--- SiembrasUi/src/app/app-routing.module.ts
+++ SiembrasUi/src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { ArbolesComponent } from './arboles/arboles.component'
+import { MunicipiosComponent } from './municipios/municipios.component'
+import { VeredasComponent } from './veredas/veredas.component'
+import { ContratistasComponent } from './contratistas/contratistas.component'
+import { SiembrasComponent } from './siembras/siembras.component'
+
+const routes: Routes = [
+  {path: 'arboles', component:ArbolesComponent},
+  {path: 'siembras', component:SiembrasComponent},
+  {path: 'contratistas', component:ContratistasComponent},
+  {path: 'veredas', component:VeredasComponent},
+  {path: 'municipios', component:MunicipiosComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
